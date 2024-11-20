75e82f2ccf549b0b2f2093bc906b390a3cbe5578
Steve Calla <callasteven@gmail.com>
2024-11-19 23:45:35 -0700

    Adjust server ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,28 @@
-const forceDatabaseRefresh = false;
-
 import express from 'express';
 import sequelize from './config/connection.js';
-import {routes} from './routes/index.js';
+import { routes } from './routes/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const forceDatabaseRefresh = false;
 
-// Serves static files in the entire client's dist folder
+// Serve static files from the 'dist' folder in the client directory
 app.use(express.static('../client/dist'));
 
+// Middleware for parsing JSON bodies
 app.use(express.json());
+
+// API routes
 app.use(routes);
 
-sequelize.sync({force: forceDatabaseRefresh}).then(() => {
+// Catch-all route to handle client-side routing (React Router)
+app.get('*', (_req, res) => {
+  // res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
+  res.sendFile('../client/dist/index.html');
+});
+
+// Sync database and start the server
+sequelize.sync({ force: forceDatabaseRefresh }).then(() => {
   app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
   });
