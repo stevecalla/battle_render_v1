3f850f3b25ea119e100cb4bde6added9455db491
Steve Calla <callasteven@gmail.com>
2024-11-19 22:59:58 -0700

    Adjust vit config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
+    port: 3000,
+    open: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
         changeOrigin: true,
-      }, // Proxy API requests to your back-end server
-      '/auth': {
-        target: 'http://localhost:3001',
+        secure: false,
+      },
+      '/auth': { target: 'http://localhost:3001',
         changeOrigin: true,
-      }, // Proxy API requests to your back-end server
-      
+        secure: false,
+      }
     },
   },
-})
+});
